b2bcbb74c923e95849a034246d6892c23f4a82eb
Dan McKeown <dangmckeown@users.noreply.github.com>
2015-12-21 22:27:34 +0000

    Update index.php
    Make this damn thing more PHPish

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
-<!doctype html>
-<html>
+<?php
+function greeting(){
+        return "Hello";
+}
+?>
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -11,10 +14,18 @@
         </head>
         <body>
 
-<h1>dangmckeown</h1>
+<h1>
+        <?php
+        echo greeting();
+        ?>
+        dangmckeown</h1>
 <img alt="Me with Vincent van Gogh" src="https://avatars0.githubusercontent.com/u/11213615?v=3&s=200">
 <h2>A work in progress</h2>
 <p><a href="https://github.com/dangmckeown">Check me out on GitHub &raquo;</a></p>
 
         </body>
-    </html>
+<?
+
+//end of file
+
+?>
